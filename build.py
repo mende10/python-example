01f9f51e04fd9cf3dcba47d940ed016f47d541a7
= <=>
2024-01-31 12:45:09 -0600

    Adding tests

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-
+use_plugin("python.install_dependencies")
 
 name = "python-example"
 default_task = "publish"
@@ -14,4 +14,6 @@ default_task = "publish"
 
 @init
 def set_properties(project):
+    project.set_property("coverage_break_build", False)
+    project.depends_on("mockito")
     pass
